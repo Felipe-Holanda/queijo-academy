13f9b8b48972ff873ce508cad491c711cca527d4
Felipe Holanda <93724958+Felipe-Holanda@users.noreply.github.com>
2022-05-30 17:52:19 -0300

    Arquivos
    Minha atividade da primeira semana de Sprint da @Kenzie-Academy-Brasil

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('especial').style.display = 'none';
+
+function fecha_nav() {
+    document.getElementById('newsletter-form').style.display = 'none';
+    document.getElementById('nav_fecha').style.display = 'none';
+    document.getElementById('especial').style.display = '';
+}
